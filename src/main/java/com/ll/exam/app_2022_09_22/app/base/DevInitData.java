a60489859d9e500f2322b9d7731db4af14036861
seeunla <107592967+seeunla@users.noreply.github.com>
2022-09-26 13:07:42 +0900

    샘플 상품옵션 생성, 각 상품당 4개씩

--- src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
@@ -2,10 +2,13 @@ package com.ll.exam.app_2022_09_22.app.base;
 
 import com.ll.exam.app_2022_09_22.app.member.entity.Member;
 import com.ll.exam.app_2022_09_22.app.member.service.MemberService;
+import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
 import com.ll.exam.app_2022_09_22.app.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -21,7 +24,7 @@ public class DevInitData {
             Member member3 = memberService.join("user3", password, "user3@test.com");
             Member member4 = memberService.join("user4", password, "user4@test.com");
 
-            productService.create("단가라 OPS", 65000, "청평화 A-1-15");
+            productService.create("단가라 OPS", 65000, "청평화 A-1-15", Arrays.asList(new ProductOption("RED", "44"), new ProductOption("BLUE", "44")));
         };
     }
 }
--- src/main/java/com/ll/exam/app_2022_09_22/app/product/entity/Product.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/product/entity/Product.java
@@ -1,12 +1,19 @@
 package com.ll.exam.app_2022_09_22.app.product.entity;
 
 import com.ll.exam.app_2022_09_22.app.base.entity.BaseEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
 @Getter
@@ -17,4 +24,15 @@ public class Product extends BaseEntity {
     private int price;
     private String name;
     private String makerShopName;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "product", cascade = ALL, orphanRemoval = true)
+    private List<ProductOption> productOptions = new ArrayList<>();
+
+    public void addOption(ProductOption option) {
+        option.setProduct(this);
+        option.setPrice(getPrice());
+
+        productOptions.add(option);
+    }
 }
