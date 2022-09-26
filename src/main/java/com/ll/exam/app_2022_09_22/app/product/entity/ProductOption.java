aaf417581eb90abc4f64607864486780fd2d03e4
seeunla <107592967+seeunla@users.noreply.github.com>
2022-09-26 15:19:59 +0900

    권장판매가, 판매가, 노출용 색상 및 사이즈, 도매가 추가

--- src/main/java/com/ll/exam/app_2022_09_22/app/product/entity/ProductOption.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/product/entity/ProductOption.java
@@ -21,15 +21,27 @@ import static javax.persistence.FetchType.LAZY;
 public class ProductOption extends BaseEntity {
     private String color;
     private String size;
+    private String displayColor;
+    private String displaySize;
+    private int salePrice;
     private int price;
+    private int wholesalePrice;
     @ManyToOne(fetch = LAZY)
+    @ToString.Exclude
     private Product product;
     private boolean isSoldOut; // 사입처에서의 품절여부
     private int stockQuantity; // 쇼핑몰에서 보유한 물건 개수
 
     public ProductOption(String color, String size) {
         this.color = color;
+        this.displayColor = color;
         this.size = size;
+        this.displaySize = size;
     }
 
+    public boolean isOrderble(int quantity) {
+        if (isSoldOut() == false) return true;
+
+        return getStockQuantity() >= quantity;
+    }
 }
